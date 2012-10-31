2a2907321eeb9cdfce71e01bcd9ee25395ebd37e
Wai Yong Low <waiyong.low.wy@gmail.com>
2012-10-31 14:28:59 -0400

    added the actie tab highlighting

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,22 +4,27 @@
 function MusicCtrl($scope){
   $scope.hello = "hello world!";
   $scope.url = "partials/navigation.html";
+  $scope.musicNav = "active";
 }
 
 function KarateCtrl($scope){
  $scope.url = "partials/navigation.html";
+ $scope.karateNav = "active";
 }
 
 function GalleryCtrl($scope){
   $scope.url = "partials/navigation.html";
+  $scope.galleryNav = "active";
 }
 
 function AboutCtrl($scope){
   $scope.url = "partials/navigation.html";
+  $scope.aboutNav = "active";
 }
 
 function SupportCtrl($scope){
   $scope.url = "partials/navigation.html";
+  $scope.supportNav = "active";
 }
 
 //MyCtrl2.$inject = [];
