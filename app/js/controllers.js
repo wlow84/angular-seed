8042daa137ddb78398d1d4872c4e5d463874148b
Wai Yong Low <waiyong.low.wy@gmail.com>
2012-11-02 16:14:57 -0400

    finished up the youtube shizz

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,9 +7,24 @@ function MusicCtrl($scope){
   $scope.musicNav = "active";
 }
 
-function KarateCtrl($scope){
+function KarateCtrl($scope,$http){
  $scope.url = "partials/navigation.html";
+ $scope.selectedVideo = "wXywsofPYt4";
+ $scope.selectedVideoTitle = "Da Strongest";
  $scope.karateNav = "active";
+
+ $scope.replaceModalVideo = function(url,title){
+  $scope.selectedVideo = url;
+  $scope.selectedVideoTitle = title;
+ }
+ $http.get('https://gdata.youtube.com/feeds/api/videos?author=bakemono444&alt=json&v=2')
+      .success(function(data){
+        $scope.videos = data.feed.entry;
+      });
+ $scope.getVideoId = function(tag){
+    var arr = tag.split(':');
+    return arr[arr.length-1];
+ }
 }
 
 function GalleryCtrl($scope, $http){
